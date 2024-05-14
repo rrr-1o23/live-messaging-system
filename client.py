3f830adbc8012648700252384486d39b3938e9ce
Ryosuke Matsushita <im23d067@gmail.com>
2024-05-14 17:57:08 +0900

    server.py, client.py のTCP通信の追加追加(ver1)

--- client.py
+++ client.py
@@ -1,7 +1,89 @@
 import socket
 import threading
 import time
+import sys
 
+class TCPClient:
+    def __init__(self, server_address, server_port):
+        self.server_address = server_address
+        self.server_port = server_port
+        self.TOKEN_MAX_BYTE = 255
+
+    def protocol_header(self, room_name, operation, state, payload):
+        room_name_size = len(room_name)
+        payload_size = len(payload)
+        return room_name_size.to_bytes(1, "big") + operation.to_bytes(1, "big") + state.to_bytes(1,"big") + payload_size.to_bytes(29, "big")
+    
+    def tcp_main(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # サーバーが待ち受けているポートにソケットします．
+        print("connecting to {}".format(self.server_address, self.server_port))
+
+        try:
+            # 接続後，サーバーとクライアントが相互に読み書きできるようになります．
+            sock.connect((self.server_address, self.server_port))
+        except socket.error as err:
+            print("Error connectiong to server: ", err)
+            return
+            #print(err)
+            #sys.exit(1)
+
+        print("接続できました")
+
+        try:
+            user_name = input("ユーザー名を入力してください -> ")
+
+            operation = int(input("1または2を入力してください (1: ルーム作成, 2: ルームに参加) -> "))
+
+            if (operation == 1):
+                room_name = input("ルーム名を入力してください -> ")
+            else:
+                room_name = " "
+                
+            state = 0
+            payload = user_name
+
+            header = self.protocol_header(room_name, operation, state, payload)
+            room_name_bytes = room_name.encode("utf-8")
+            payload_bytes = payload.encode("utf-8")
+
+            data = header + room_name_bytes + payload_bytes
+
+            '''
+            print(----------------)
+            print("header: ", header)
+            print("\n")
+            print("data: ", data)
+            print(----------------)
+            '''
+
+            if (operation == 1):
+                sock.send(data) # ヘッダー + ボディ を送信
+                token = sock.recv(self.TOKEN_MAX_BYTE)
+            else:
+                sock.send(data) # ヘッダー + ボディ を送信
+                room_name_list = sock.recv(4096)
+                print(room_name_list)
+                room_name = input("参加したいルーム名を入力してください -> ")
+                room_name_bytes = room_name.encode("utf-8")
+                sock.send(room_name_bytes)
+                token = sock.recv(self.TOKEN_MAX_BYTE)
+                
+
+
+            print("tokenを受け取りました -> ", token)
+
+        except socket.error as err:
+            print("Socket error: ", err)
+        except Exception as e:
+            print("Error: ", e)
+
+        finally:
+            print("closing socket")
+            sock.close()
+        
+'''
 class UDPClient:
     def __init__(self, server_address, server_port):
         self.server_address = server_address
@@ -9,10 +91,11 @@ class UDPClient:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.client_address = ''
         self.client_port = self.find_free_port()
+        self.client_info = str(self.client_address)+ "," + str(self.client_port)
         self.sock.bind((self.client_address, self.client_port))
         self.username = ''
     
-    # サーバに接続後，最初にusernameを入力
+    # サーバに接続後,最初にusernameを入力
     def input_username(self):
             USER_NAME_MAX_BYTE_SIZE = 255
             self.username = input("Please enter your username: ") # username入力
@@ -41,15 +124,22 @@ class UDPClient:
             usernamelen = len(self.username).to_bytes(1, byteorder='big')# UTF-8 エンコーディングを使用して変換 (指定されたバイト数（ここでは1バイト）のバイト列に変換します)
             data = usernamelen + self.username.encode() + message.encode() # ユーザー名とメッセージを結合して送信
             # サーバへのデータ送信
-            self.sock.sendto(data, (server_address, server_port))
+            self.sock.sendto(data, (self.server_address, self.server_port))
             time.sleep(0.1)
 
     # 他ユーザのメッセージの受信処理
     def receive_message(self):
         while True:
             rcv_data = self.sock.recvfrom(4096)[0].decode("utf-8")
-            print(rcv_data)
-             
+
+            if (rcv_data == "Timeout!"): # タイムアウト処理
+                print(rcv_data)
+                self.sock.close()
+                sys.exit()
+            else:
+                print(rcv_data)
+
+
     # 空きポートの割り当て
     def find_free_port(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -68,10 +158,15 @@ class UDPClient:
         thread_receive.start()
         thread_send.join()
         thread_receive.join()
-
+'''
 
 if __name__ == "__main__":
+
     server_address = '0.0.0.0'
     server_port = 9001
-    client = UDPClient(server_address, server_port)
-    client.start()
+
+    client = TCPClient(server_address, server_port)
+    client.tcp_main()
+
+    #client = UDPClient(server_address, server_port)
+    #client.start()
--- server.py
+++ server.py
@@ -1,24 +1,103 @@
 import socket
-'''
-- アドレス情報をリストで保存しているが, ユーザー名とアドレスをハッシュマップで管理するように今後変更
-- 各クライアントの最後のメッセージ送信時刻を追跡
+import threading
+import time
+import secrets
+
+class TCPServer:
+    def __init__(self, server_address, server_port):
+        self.server_address = server_address
+        self.server_port = server_port
+        self.HEADER_MAX_BYTE = 32
+        self.TOKEN_MAX_BYTE = 255
+        self.client_map = {} # {client_address : [token, payload(username), host(0:guest, 1:host), room_name]}
+        self.room_member_map = {} # {room_name : [token, client_address]}
+        
+
+    def tcp_main(self):
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind((self.server_address, self.server_port)) # server_address: 0.0.0.0 server_port: 9002
+        sock.listen(1)
+        print("server listening...")
+        
+
+        while True:
+            connection, client_address = sock.accept()
+            try:
+                print("connection from", client_address)
+                '''
+                while True:
+                    data = connection.recv(4096)  # 1024バイトごとにデータを受け取る（適宜サイズを調整）
+                    if not data:
+                        break  # データを受け取らなくなったらループを抜ける
+                '''
+                data = connection.recv(4096) # ヘッダー+ボディ の受け取り
+
+                header = data[:self.HEADER_MAX_BYTE]
+                #print("header: ", header)
+                
+                room_name_size = int.from_bytes(header[:1], "big")
+                operation = int.from_bytes(header[1:2], "big")
+                state = int.from_bytes(header[2:3], "big")
+                payload_size = int.from_bytes(header[3:self.HEADER_MAX_BYTE], "big")
+
+
+                print("------------------------------")
+                print("room_name_size: ", room_name_size)
+                print("operation: ", operation)
+                print("state: ", state)
+                print("payload_size: ", payload_size)
+
+                print("------------------------------")
+                print("Recive header from client.")
+                print("Byte length: RoomNameSize: {}, Operation: {}, State: {}, PayloadSize: {}".format(room_name_size, operation, state, payload_size))
+                print("------------------------------")
+
+                body = data[self.HEADER_MAX_BYTE:]
+                room_name = body[:room_name_size].decode("utf-8")
+                payload = body[room_name_size:room_name_size + payload_size].decode("utf-8") # username
+
+                # print("body: ", body)
+                print("room_name: ", room_name)
+                print("payload: ", payload)
+                
+                #print(self.client_token)
+
+                token = secrets.token_bytes(self.TOKEN_MAX_BYTE)
+
+                if (operation == 1):
+                    host = 1
+                    connection.send(token)
+                    self.room_member_map[room_name] = [[token, client_address]]
+                    print("新しいルームを作成します (Host: {})".format(payload))
+
+                elif (operation == 2):
+                    host = 0
+                    connection.send(str(self.room_member_map.keys()).encode("utf-8"))
+                    room_name = connection.recv(4096).decode("utf-8")
+                    print("room_nameを受け取りました -> ", room_name)
+                    self.room_member_map[room_name].append([token, client_address])
+                    connection.send(token)
+                    print(self.room_member_map)
+                    print("既存のルームに参加します")
+
+                self.client_map.update({client_address : [token, payload, host, room_name]})
+
+            except Exception as e:
+                print('Error: ' + str(e))
+            
+            finally:
+                connection.close()
+            
 
 '''
-
-def receive_message(data):
-    # クライアントからのデータを1バイト受信します。ここで、1バイトはユーザー名の長さを表す値です。
-    # この値を整数に変換するために [0] を使用してバイト列の最初の要素を取得します
-    usernamelen = data.recvfrom(1)[0]
-    # ユーザ-名を名を受信
-    username = data.recvfrom(usernamelen).decode()
-    # メッセージを受信
-    message = data.recvfrom(4096).decode()
 
 class UDPServer:
     def __init__(self, server_address, server_port):
         self.server_address = server_address
         self.server_port = server_port
-        self.clients = []
+        # self.clients = []
+        self.clientsmap = {}
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind((self.server_address, self.server_port))
         print("サーバが起動しました")
@@ -28,14 +107,20 @@ class UDPServer:
     # クライアントからのメッセージ受信処理 及び 他ユーザへの送信
     def handle_message(self):
         while True:
-            data, client_address = self.sock.recvfrom(4096) # 最大4096byteのデータを受信
 
+            data, client_address = self.sock.recvfrom(4096) # 最大4096byteのデータを受信
             # 新しいクライアントからの受信 -> clientsリストにアドレスを保存
-            if (not client_address in self.clients):
-                print("新しいユーザーです -> ", data.decode('utf-8'))
-                self.clients.append(client_address)
+            if (not client_address in self.clientsmap):
+                name = data.decode('utf-8')
+                # print("新しいユーザーです -> ", name)
+                # self.clients.append(client_address)
+                self.clientsmap.update({client_address : [name, time.time()]})
+                #print(client_address)
+                #print("新しいユーザーです -> ", self.clientsmap[client_address], time.ctime())
+                print("新しいユーザーです -> ", name)
 
             else:
+                self.clientsmap[client_address][1] = time.time() # クライアントの最終送信時刻を更新
                 usernamelen = data[0] # 最初のバイトがユーザー名の長さを表す
                 username = data[1:usernamelen + 1].decode() # ユーザー名を取得
                 message = data[usernamelen + 1:].decode() # メッセージを取得
@@ -44,16 +129,38 @@ class UDPServer:
     
     # リレーシステム
     def relay_message(self, message):
-        for client_address in self.clients:
+        for client_address in self.clientsmap.keys():
             self.sock.sendto(message.encode(), client_address)
+    
+    # 各クライアントの最後の最後のメッセージ送信時刻を追跡
+    def send_time_tracking(self):
+        while True:
+            time.sleep(60)# 1分単位で追跡
+            print("スリープ解除")
+            try:
+                for address, value in self.clientsmap.items():
+                    send_time = value[1]
+                    if (time.time() - send_time > 300): # クライアントからの通信が5分なければclientsmapから情報を削除
+                        self.clientsmap.pop(address)
+                        self.sock.sendto("Timeout!".encode(), address)
+                        print(value[0], address, "connection has been lost.")
+            except Exception as e: # ハッシュマップの中身がない時のエラー処理
+                pass
 
     def start(self):
-        self.handle_message()
-
+        thread_main = threading.Thread(target = self.handle_message)
+        thread_tracking = threading.Thread(target = self.dsend_time_tracking)
+        thread_main.start()
+        thread_tracking.start()
+        thread_main.join()
+        thread_tracking.join()
+'''
 if __name__ == "__main__":
     server_address = '0.0.0.0'
     server_port = 9001
     print('starting up on port {}'.format(server_port))
-    server = UDPServer(server_address, server_port)
-    server.start()
+    server = TCPServer(server_address, server_port)
+    server.tcp_main()
 
+    #server = UDPServer(server_address, server_port)
+    #server.start()
